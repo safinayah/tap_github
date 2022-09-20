ae2483e37e8095ee062cf8724a9f9f2683e716d0
UPDATE <amerknane57@gmail.com>
2022-09-20 12:10:25 +0300

    added a founction get pull requests

--- pull_request.py
+++ pull_request.py
@@ -1,14 +1,6 @@
 import json
 
-_url = "https://api.github.com/repos/amerknane/ameerkn/pulls"
-
-_headers = {
-    # "post": "/repos/amerknane/ameerkn/ameerkn/pulls",
-    "Accept": "application/vnd.github+json",
-    "Authorization": "Bearer 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",
-
-}
-# _d={
+# body={
 # "title":"Amazing new feature",
 # "body":"Please pull these awesome changes in!",
 # "head":"octocat:new-feature",
@@ -16,6 +8,22 @@ _headers = {
 # }
 import requests
 
+with open("config.json", 'r') as f:
+    data_conf = f.readlines()
+token = data_conf["access_token"]
+owner_name = data_conf["Owner_name"]
+repo_name = data_conf["repo_name"]
 
-response = requests.get(_url,_headers)
-print(response.json())
+
+
+def get_pull_requests(token, owner_name, repo_name):
+    _url = "https://api.github.com/repos/{}/{}/pulls".format(owner_name, repo_name)
+
+    _headers = {
+        # "post": "/repos/amerknane/ameerkn/ameerkn/pulls",
+        "Accept": "application/vnd.github+json",
+        "Authorization": "Bearer {}".format(token),
+
+    }
+    response = requests.get(_url, _headers)
+    return response
