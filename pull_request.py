9d1c0800385eca8ddc86d88fea8514b65223684b
UPDATE <amerknane57@gmail.com>
2022-09-20 15:18:22 +0300

    pull request api created

--- pull_request.py
+++ pull_request.py
@@ -1,36 +1,39 @@
 import json
-
+import requests
 # body={
 # "title":"Amazing new feature",
 # "body":"Please pull these awesome changes in!",
 # "head":"octocat:new-feature",
 # "base":"master"
 # }
-import requests
+
+
 
 def read_config():
     with open("config.json", 'r') as f:
-        data_conf = f.readlines()
+        data_conf = json.load(f)
+
     return data_conf
 
 
-_headers = {
+
+
+
+def get_pull_requests(data):
+    _url = "https://api.github.com/repos/{}/{}/pulls".format(data['Owner_name'],data['repo_name'])
+    print(_url)
+    _headers = {
         # "post": "/repos/amerknane/ameerkn/ameerkn/pulls",
         "Accept": "application/vnd.github+json",
-        "Authorization": "Bearer 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",
+        "Authorization": "Bearer {}".format(data),
 
     }
-def get_pull_requests(data):
-    _url = "https://api.github.com/repos/{}/{}/pulls".format(data['access_token'],data['repo_name'])
-
-
     response = requests.get(_url, _headers)
 
-    return response
+    return response.json()
+
 
 if __name__ == '__main__':
-    data=read_config()
-    get_pull_requests(data)
-
-
-
+    data = read_config()
+    response = get_pull_requests(data)
+    print(response)
